71ba282e8bee138becae53fd7323d9b4f912df18
Muhammad Jamshaid Ghaffar <muhammadjamshaid89@gmail.com>
2021-07-17 11:47:30 +0500

    added turn and hold functionality

--- script.js
+++ script.js
@@ -16,21 +16,48 @@ class Player{
     }
     roll(){
         let number = getRndInteger(1,6);
+        showDice(number);
         if (number == 1)
         {
             this.switchTurn();
             return;
         }
-        this.currentScore += number ; 
-        this.currentScoreElement.innerHTML = this.currentScore.toString();    
+        this.changeCurrentScore(this.currentScore+number);    
+    }
+    hold(){
+        this.changeScore(this.score+this.currentScore);
+        if(this.score >= 10)
+        {
+            console.log("player " +  turn + "wins");
+        }
+        this.switchTurn();
     }
     switchTurn(){
         if  (turn == 1)
+        {
             turn = 2;
+            panel[1].classList.remove("fade");
+            panel[0].classList.add("fade");
+        }
+            
         else 
+        {
             turn = 1;
+            panel[0].classList.remove("fade");
+            panel[1].classList.add("fade");
+        }
+            
         this.currentScore = 0 ; 
         this.currentScoreElement.innerHTML = "0";
+        
+    }
+    changeScore(score){
+        this.score = score;
+        this.scoreElement.innerHTML = this.score.toString();
+    }
+    changeCurrentScore(currentScore){
+        this.currentScore = currentScore ; 
+        this.currentScoreElement.innerHTML = this.currentScore.toString(); 
     }
 }
 
@@ -40,12 +67,17 @@ let player1 = undefined;
 let player2 = undefined;
 
 let turn = 1 ;
+let diceImage = document.getElementById('dice-image');
+let panel = document.querySelectorAll(".panel");
+
 // ########## initializing global variables #############
 {
     let score = document.querySelectorAll('.score');
     let currentScore = document.querySelectorAll(".current-score");
-    player1 = new Player (score[0] , currentScore[0]);
-    player2 = new Player (score[1] , currentScore[1]);
+    
+    player1 = new Player (score[0] , currentScore[0] );
+    player2 = new Player (score[1] , currentScore[1] );
+    
 }
 
 // ########## UTLILITY FUNCTIONS #############
@@ -64,6 +96,40 @@ function roll(){
     else
         player2.roll();
 }
+function hold(){
+    if (turn == 1)
+        player1.hold();
+    else
+        player2.hold();
+}
 
+function showDice(number){
+    switch(number){
+        case 1:
+            diceImage.src = "dice-1.png";
+            break;
+        case 2:
+            diceImage.src = "dice-2.png";
+            break;
+        case 3:
+            diceImage.src = "dice-3.png";
+            break;
+        case 4:
+            diceImage.src = "dice-4.png";
+            break;
+        case 5:
+            diceImage.src = "dice-5.png";
+            break;
+        case 6:
+            diceImage.src = "dice-6.png";
+            break;
+    }
+}
+function win(){
+    
+}
+function endGame(){
+    document.getElementById('roll-btn').disabled = true;
+}
 // ########## CALLING FUNCTIONS ###########
 newGame();
